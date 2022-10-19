365f144b93056b2f5f080ab7443f2deb41605241
shivam pandey <shivm9899@gmail.com>
2022-10-19 23:03:59 +0530

    sql table structure

---
+++ SQL_Script_and_Table_Structure.sql
@@ -0,0 +1,155 @@
+-- Script to create the Product table and load data into it.
+
+DROP TABLE product;
+CREATE TABLE product
+( 
+    product_category varchar(255),
+    brand varchar(255),
+    product_name varchar(255),
+    price int
+);
+
+INSERT INTO product VALUES
+('Phone', 'Apple', 'iPhone 12 Pro Max', 1300),
+('Phone', 'Apple', 'iPhone 12 Pro', 1100),
+('Phone', 'Apple', 'iPhone 12', 1000),
+('Phone', 'Samsung', 'Galaxy Z Fold 3', 1800),
+('Phone', 'Samsung', 'Galaxy Z Flip 3', 1000),
+('Phone', 'Samsung', 'Galaxy Note 20', 1200),
+('Phone', 'Samsung', 'Galaxy S21', 1000),
+('Phone', 'OnePlus', 'OnePlus Nord', 300),
+('Phone', 'OnePlus', 'OnePlus 9', 800),
+('Phone', 'Google', 'Pixel 5', 600),
+('Laptop', 'Apple', 'MacBook Pro 13', 2000),
+('Laptop', 'Apple', 'MacBook Air', 1200),
+('Laptop', 'Microsoft', 'Surface Laptop 4', 2100),
+('Laptop', 'Dell', 'XPS 13', 2000),
+('Laptop', 'Dell', 'XPS 15', 2300),
+('Laptop', 'Dell', 'XPS 17', 2500),
+('Earphone', 'Apple', 'AirPods Pro', 280),
+('Earphone', 'Samsung', 'Galaxy Buds Pro', 220),
+('Earphone', 'Samsung', 'Galaxy Buds Live', 170),
+('Earphone', 'Sony', 'WF-1000XM4', 250),
+('Headphone', 'Sony', 'WH-1000XM4', 400),
+('Headphone', 'Apple', 'AirPods Max', 550),
+('Headphone', 'Microsoft', 'Surface Headphones 2', 250),
+('Smartwatch', 'Apple', 'Apple Watch Series 6', 1000),
+('Smartwatch', 'Apple', 'Apple Watch SE', 400),
+('Smartwatch', 'Samsung', 'Galaxy Watch 4', 600),
+('Smartwatch', 'OnePlus', 'OnePlus Watch', 220);
+COMMIT;
+
+
+
+
+-- All the SQL Queries written during the video
+
+select * from product;
+
+
+-- FIRST_VALUE 
+-- It did not reduce the data, will show all results
+-- Write query to display the most expensive product under each category (corresponding to each record)
+
+SELECT *,
+first_value(product_name) OVER(PARTITION BY product_category ORDER BY price DESC) as "most_expensive",
+row_number() OVER(PARTITION BY product_category ORDER BY price DESC) as "ranK_expensive"
+FROM product 
+
+-- Write query to display the most expensive product under each category
+SELECT product_category, most_expensive
+FROM 
+	(SELECT *, 
+	first_value(product_name) OVER(PARTITION BY product_category ORDER BY price DESC) as "most_expensive"
+	from product) as tabl
+GROUP BY product_category, most_expensive
+
+
+
+-- LAST_VALUE 
+-- Write query to display the least expensive product under each category (corresponding to each record)
+-- frame clause must be included in nth_value() and last_value()
+-- DEFAULT frame clause - "range between unbounded preceding and current row"
+-- change frame clause when used with nth_value() and last_value() "range between unbounded preceding and unbounded following"
+select *,
+first_value(product_name) 
+    over(partition by product_category order by price desc) 
+    as most_exp_product,
+last_value(product_name) 
+    over(partition by product_category order by price desc
+        range between unbounded preceding and unbounded following) 
+    as least_exp_product    
+from product
+WHERE product_category ='Phone';
+
+
+-- Alternate way to write SQL query using Window functions
+-- it helps when we are using multiple window clause
+SELECT *,
+first_value(product_name) OVER w as "most_expensive",
+last_value(product_name) OVER w as "least_expensive"
+FROM product
+WINDOW w as(PARTITION BY product_category ORDER BY price DESC
+			RANGE between unbounded preceding and unbounded following)
+            
+
+-- NTH_VALUE 
+-- Write query to display the Second most expensive product under each category.
+-- frame clause must be added to get actual value otherwise it will only look above previous rows of current row
+select *,
+first_value(product_name) over w as most_exp_product,
+last_value(product_name) over w as least_exp_product,
+nth_value(product_name, 5) over w as second_most_exp_product
+from product
+window w as (partition by product_category order by price desc
+            range between unbounded preceding and unbounded following);
+
+
+
+-- NTILE
+-- it create bucket on the basis on column defined with order by clause inside window clause.
+-- it tries to create equal number of buckets , if not give importance to first bucket.
+
+-- Write a query to segregate all the expensive phones, mid range phones and the cheaper phones.
+select x.product_name, 
+case when x.buckets = 1 then 'Expensive Phones'
+     when x.buckets = 2 then 'Mid Range Phones'
+     when x.buckets = 3 then 'Cheaper Phones' END as Phone_Category
+from (
+    select *,
+    ntile(3) over (order by price desc) as buckets
+    from product
+    where product_category = 'Phone') x;
+
+
+
+
+-- CUME_DIST (cumulative distribution) ; 
+/*  Formula = Current Row no (or Row No with value same as current row) / Total no of rows */
+
+-- Query to fetch all products which are constituting the first 30% 
+-- of the data in products table based on price.
+select product_name, cume_dist_percetage
+from (
+    select *,
+    cume_dist() over (order by price desc) as cume_distribution,
+    round(cume_dist() over (order by price desc)::numeric * 100,2)||'%' as cume_dist_percetage
+    from product) x
+where x.cume_distribution <= 0.3;
+
+
+
+
+-- PERCENT_RANK (relative rank of the current row / Percentage Ranking)
+/* Formula = Current Row No - 1 / Total no of rows - 1 */
+
+-- Query to identify how much percentage more expensive is "Galaxy Z Fold 3" when compared to all products.
+-- 80% products are less expensive then Galaxy Z Fold 3
+select product_name, per
+from (
+    select *,
+    percent_rank() over(order by price) ,
+    round(percent_rank() over(order by price)::numeric * 100, 2) as per
+    from product) x
+where x.product_name='Galaxy Z Fold 3';
+
